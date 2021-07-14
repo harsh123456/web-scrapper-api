07883d2f6b419f5ef3eda6c8152a2f25f4449999
Harsh Kansara <harshkansara98@gmail.com>
2021-07-15 07:47:24 +1200

    Creating the product Offers section.

--- index.js
+++ index.js
@@ -40,5 +40,20 @@ app.get('/products/:productId/reviews', async (req, res) => {
     }
 });
 
+//GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
